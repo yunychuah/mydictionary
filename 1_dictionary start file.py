b8eebe8d991bb287ffdac94b8c896c5f4a045863
Yun <yun_chuah1@baylor.edu>
2022-09-08 12:14:26 -0500

    worked on start file

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,16 +10,27 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook)) #type allows you to know what type of object u r dealing w 
 
+phone = phonebook['Chris']
 
+print(phone)
 
+print(len(phonebook)) #will show 3 if there are 3 elements in the phonebook
+
+mydictionary = dict(m=8, n=9) #dict funciton, m=8 is a key value pair, when you print it out u see that values are m and n and are strings and keys are 8 and 9 and integers
+
+print(mydictionary)
+
+mydict = {} #creates an empty dictionary and u can keep adding to it 
+
+print(mydict)
 
 print()
 print('*****  end section 1 ********')
 print()
 
-
-'''
 
 
 print()
@@ -27,9 +38,12 @@ print('*****  start section 2 - search dictionary ********')
 print()
 
 
+name = 'Chris'
 
-
-
+if name in phonebook: 
+    print(phonebook[name]) 
+else:
+    print(name, 'not found') #if you can't find the name in phonebook 
 
 
 print()
@@ -47,6 +61,9 @@ print('*****  start section 3 - edit/append dictionary ********')
 print()
 
 
+print(phonebook)
+phonebook['Chris'] = '555-0123' #b/c chris exists in dictionary, so it will update from 1111 to 0123
+phonebook['Joe'] = '555-4444' #Joe can't be found in dictionary so will add entire key value pair to dictionary
 
 
 
@@ -56,13 +73,13 @@ print()
 
 
 
-
-
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+
+del phonebook['Chris']
+print(phonebook)
 
 
 
@@ -72,17 +89,26 @@ print()
 
 
 
-
-
-
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
+for key in phonebook: #key is not a reserved word u can change key to k and as long as u change the other 2 keys below to k it will still produce results
+    print(key) #defalut option ot iterate through dictionary is to iterate through keys so if u want to iterate vlaues u hv to specifically call it 
+    print(phonebook[key])#prints out corresponding values(phone numbers) to katie joanne and joe 
 
 
+for value in phonebook.values(): #.values b/c u want it to cycle through all the values to find the phone number
+    print(value)
 
 
+for k,v in phonebook.items(): #reminder that k and v are just variable names and u can name these whatever 
+    print('key:',k, ' value: ', v)
+
+
+for tuple in phonebook.items(): #for if you did not split it up 
+    print(tuple) #if method returns a tuple u can split it up by giving it multiple variables, know its a tuple b/c of the parentheses which means these are immutable objects(Can't change them)
+                    #in dictionaries keys are mutable but values are immutable 
 
 print()
 print('*****  end section 5 ********')
@@ -94,10 +120,13 @@ print()
 
 print()
 print('*****  start section 6 - using get and clear ********')
-print()
+print() #clear method clears out all the elements 
 
+phone = phonebook.get('Chris', 'key not found') #warning this is case sensitive so no chris only Chris uppercase
+print(phone)
 
-
+phonebook.clear()
+print(phonebook)
 
 
 
@@ -109,9 +138,12 @@ print()
 
 print()
 print('*****  start section 7 - using pop method ********')
-print()
+print() #allows u to remove key value pair from your dictionary 
 
+remove = phonebook.pop('Chris' , 'key not found')
 
+print(remove)
+print(phonebook)
 
 
 
@@ -126,8 +158,10 @@ print()
 print('*****  start section 8 - using popitem ********')
 print()
 
+a = phonebook.popitem()
 
-
+print(a)
+print(phonebook)
 
 
 
@@ -136,23 +170,22 @@ print('*****  end section 8 ********')
 print()
 
 
-
+''''''
 print()
 print('*****  start section 9 - using random and converting to list ********')
-print()
+print() #use random aspect of a list 
 
 
+list_of_keys = list(phonebook)
+print(list_of_keys)
+random_key = random.choice(list_of_keys)
+print(random_key)
+print(phonebook[random_key])
 
+print(phonebook[random.choice(list(phonebook))])
 
 
 print()
 print('*****  end section 9 ********')
 print()
-
-
-'''
-
-
-
-
 
